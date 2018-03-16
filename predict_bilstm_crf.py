5ab9194e2d2e0797d75f8b021cc526a5a3133697
Tracy Liu <yechengliu94@sina.com>
2018-03-16 14:55:52 +0800

    Update predict_bilstm_crf.py

--- predict_bilstm_crf.py
+++ predict_bilstm_crf.py
@@ -1,4 +1,4 @@
-# coding = utf-8
+# coding=utf-8
 import numpy as np
 from bilstm_crf import BiLSTM_CRF
 from collections import defaultdict
@@ -37,7 +37,7 @@ def get_y_orig(y_pred, y_true):
 def get_entity(X_data, y_data):
     """
     :param X_data: 以character_level text列表为元素的列表
-    :param y_data: 以entity;列表为元素的列表
+    :param y_data: 以entity列表为元素的列表
     :return: [{'entity': [phrase or word], ....}, ...]
     """
     n_example = len(X_data)
@@ -58,11 +58,9 @@ def get_entity(X_data, y_data):
 
     return entity_list
 
-def evaluation(pred_entity, true_entity):
+def micro_evaluation(pred_entity, true_entity):
     n_example = len(pred_entity)
-    t_pos = []
-    pred = []
-    true = []
+    t_pos, true, pred = [], [], []
     for n in range(n_example):
         et_p = pred_entity[n]
         et_t = true_entity[n]
@@ -73,11 +71,34 @@ def evaluation(pred_entity, true_entity):
         pred.extend([len(v) for v in et_p.values()])
         true.extend([len(v) for v in et_t.values()])
 
-    precision = sum(t_pos) / sum(pred)
-    recall = sum(t_pos) / sum(true)
+    precision = sum(t_pos) / sum(pred) + 1e-8
+    recall = sum(t_pos) / sum(true) + 1e-8
     f1 = 2 / (1 / precision + 1 / recall)
 
     return round(precision, 4), round(recall, 4), round(f1, 4)
+
+def macro_evaluation(pred_entity, true_entity):
+    label = ['PER', 'ORG', 'LOC']
+    n_example = len(pred_entity)
+    precision, recall, f1 = [], [], []
+    for l in label:
+        t_pos, true, pred = [], [], []
+        for n in range(n_example):
+            et_p = pred_entity[n]
+            et_t = true_entity[n]
+            print('the prediction is', et_p.items(), '\n',
+                  'the true is', et_t.items())
+            t_pos.extend([len(set(et_p[l]) & set(et_t[l]))
+                          if l in (et_p.keys() & et_t.keys()) else 0])
+            true.extend([len(et_t[l]) if l in et_t.keys() else 0])
+            pred.extend([len(et_p[l]) if l in et_p.keys() else 0])
+        precision.append(sum(t_pos) / sum(pred) + 1e-8)
+        recall.append(sum(t_pos) / sum(true) + 1e-8)
+        f1.append(2 / (1 / precision[-1] + 1 / recall[-1]))
+    avg_precision = np.mean(precision)
+    avg_recall = np.mean(recall)
+    avg_f1 = np.mean(f1)
+    return round(avg_precision, 4), round(avg_recall, 4), round(avg_f1, 4)
 
 
 if __name__ == '__main__':
@@ -88,24 +109,22 @@ if __name__ == '__main__':
     # print(X_test, X_test.shape)
     y_test = np.load('data/y_test.npy')
 
-    # ## loss:16.72; 0.5966 0.5158 0.5532
-    # ner_model = BiLSTM_CRF(n_input=200, n_vocab=embedding_mat.shape[0], n_embed=100,
-    #                        embedding_mat=embedding_mat, keep_prob=0.5,
-    #                        n_lstm=128, keep_prob_lstm=0.95, n_entity=7,
-    #                        optimizer='adam', batch_size=64, epochs=500)
+    ## loss: 16.68228; 
+    ## micro: precision:0.7741 recall:0.7641 F1:0.7691 LSTM
+    # ner_model = BiLSTM_CRF(n_input=200, n_vocab=char_embedding_mat.shape[0],
+    #                        n_embed=100, embedding_mat=char_embedding_mat,
+    #                        keep_prob=0.5, n_lstm=128, keep_prob_lstm=0.7,
+    #                        n_entity=7, optimizer='adam', batch_size=32, epochs=500)
 
-    # ## loss: 16.68706; precision:0.7474, recall:0.7302, F1:0.7387
-    # ner_model = BiLSTM_CRF(n_input=200, n_vocab=embedding_mat.shape[0], n_embed=100,
-    #                        embedding_mat=embedding_mat, keep_prob=0.5,
-    #                        n_lstm=100, keep_prob_lstm=0.8, n_entity=7,
-    #                        optimizer='adam', batch_size=64, epochs=500)
-    
-    ## loss: 16.68228; precision:0.7741 recall:0.7641 F1:0.7691
+    #loss: 16.68265;
+    # micro: precision:0.7798; recall:0.7625; F1:0.771  GRU
+    # macro: 0.7914 0.7677 0.7791
     ner_model = BiLSTM_CRF(n_input=200, n_vocab=char_embedding_mat.shape[0],
                            n_embed=100, embedding_mat=char_embedding_mat,
-                           keep_prob=0.5, n_lstm=128, keep_prob_lstm=0.7,
-                           n_entity=7, optimizer='adam', batch_size=32, epochs=500)
-    model_file = 'checkpoints/bilstm_crf_weights_best.hdf5'
+                           keep_prob=0.5, n_lstm=256, keep_prob_lstm=0.6,
+                           n_entity=7, optimizer='adam', batch_size=16, epochs=500)
+
+    model_file = 'checkpoints/bilstm_crf_best.hdf5'
     ner_model.model.load_weights(model_file)
 
     y_pred = ner_model.model.predict(X_test[:, :])
@@ -121,9 +140,10 @@ if __name__ == '__main__':
     # print(X_list)
     pred_entity, true_entity = get_entity(X_list, pred_list), get_entity(X_list, true_list)
     # print(pred_entity, true_entity)
-    precision, recall, f1 = evaluation(pred_entity, true_entity)
+    precision, recall, f1 = micro_evaluation(pred_entity, true_entity)
     print(precision, recall, f1)
-
+    
+    # Just test 'get_entity' function:
     # X_true = [['火','箭','队','的','主','场','在','休','斯','顿',',',
     #            '当','家','球','星','为','哈','登','和','保','罗'],
     #           ['北', '京', '故', '宫', '主','场','在','休','斯','顿',',',
